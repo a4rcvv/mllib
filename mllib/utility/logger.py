0f6ca82cfd01e5762107d3f4c1799e1a6b9e2ef5
arc <arcslabmail@gmail.com>
2020-04-12 11:49:31 +0900

    [CHANGE] logger.py fully
    * delete Logger class and make 2 functions which return logging.Logger

--- mllib/utility/logger.py
+++ mllib/utility/logger.py
@@ -1,129 +1,56 @@
-import inspect
 import logging
-from urllib.error import HTTPError
+import os
 
-import slackweb
+from slack_log_handler import SlackLogHandler
+
+STOP_LOG = 100
 
 
-class Logger:
-    """The class to wrap "logging" in standard library and "slackweb".
+def make_root_logger(console_loglevel: int = logging.DEBUG, file_loglevel: int = STOP_LOG,
+                     slack_loglevel: int = STOP_LOG, log_file_path: str = None) -> logging.Logger:
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.DEBUG)
 
-    """
+    formatter = logging.Formatter(
+        fmt="[%(levelname)s] %(asctime)s %(module)s::%(funcName)s, line %(lineno)d >> %(message)s"
+    )
 
-    # logging.CRITICAL is defined as 50, which is the highest value defined in logging.
-    # I will use 100 to show that a logger have not to log.
-    STOP_LOG = 100
+    if console_loglevel < STOP_LOG:
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(formatter)
+        console_handler.setLevel(console_loglevel)
+        root_logger.addHandler(console_handler)
 
-    # These values are from logger.py. I defined them here to use them without importing "logging"
-    CRITICAL = 50
-    FATAL = CRITICAL
-    ERROR = 40
-    WARNING = 30
-    WARN = WARNING
-    INFO = 20
-    DEBUG = 10
-    NOTSET = 0
+    if log_file_path is not None and file_loglevel < STOP_LOG:
+        file_handler = logging.FileHandler(log_file_path)
+        file_handler.setFormatter(formatter)
+        file_handler.setLevel(file_loglevel)
+        root_logger.addHandler(file_handler)
 
-    _slack_webhook_url: str = None
-    _slack: slackweb.Slack = None
+    if slack_loglevel < STOP_LOG:
+        env_val_name = "WEBHOOK_URL"
+        try:
+            webhook_url = os.environ[env_val_name]
+        except KeyError as e:
+            root_logger.error(
+                "Environment variable \"{0}\" is not defined, so root logger cannot log to Slack." +
+                "Do \"export {0}=(WebHook URL)\" in the terminal or" +
+                "edit environment variables in Edit Configurations, PyCharm".format(
+                    env_val_name))
+        else:
+            slack_handler = SlackLogHandler(webhook_url)
+            slack_handler.setFormatter(formatter)
+            slack_handler.setLevel(slack_loglevel)
+            root_logger.addHandler(slack_handler)
 
-    @staticmethod
-    def set_webhook(webhook_url: str) -> None:
-        """Set the Webhook URL for Slack. Class method.
+    return root_logger
 
-        Args:
-            webhook_url: Webhook URL
 
-        """
-        Logger._slack_webhook_url = webhook_url
-        Logger._slack = slackweb.Slack(webhook_url)
+def make_child_logger(logger_name: str) -> logging.Logger:
+    logger = logging.getLogger(logger_name)
+    logger.addHandler(logging.NullHandler())
+    logger.setLevel(logging.DEBUG)
 
-    def __init__(self, logger_name: str, console_loglevel: int = STOP_LOG, file_loglevel: int = STOP_LOG,
-                 slack_loglevel: int = STOP_LOG, log_file_path: str = None):
-        """
+    logger.propagate = True
 
-        Args:
-            logger_name:
-            console_loglevel:
-            file_loglevel:
-            slack_loglevel:
-            log_file_path:
-        """
-        self.logger = logging.getLogger(logger_name)
-        self.slack_loglevel = slack_loglevel
-
-        self.logger.setLevel(logging.DEBUG)
-
-        formatter = logging.Formatter(
-            fmt="%(levelname)s %(asctime)s %(message)s"
-        )
-
-        if console_loglevel < Logger.STOP_LOG:
-            console_handler = logging.StreamHandler()
-            console_handler.setFormatter(formatter)
-            console_handler.setLevel(console_loglevel)
-            self.logger.addHandler(console_handler)
-
-        if log_file_path is not None and file_loglevel < Logger.STOP_LOG:
-            file_handler = logging.FileHandler(log_file_path)
-            file_handler.setFormatter(formatter)
-            file_handler.setLevel(file_loglevel)
-            self.logger.addHandler(file_handler)
-
-    def _log_to_slack(self, loglevel: int, message: str) -> None:
-        """Send a log message to Slack.
-
-        Args:
-            loglevel: Level of logging like "logger.CRITICAL"
-            message: log message
-
-        """
-
-        if self.slack_loglevel <= loglevel:
-            if Logger._slack is None:
-                self.error("Webhook url is not defined. You must do Logger.set_webhook(url).", False)
-            else:
-                try:
-                    Logger._slack.notify(text=message)
-                except HTTPError as e:
-                    self.error("Failed to log to slack. HTTP Status is: " + e.reason, False)
-
-    @staticmethod
-    def _make_message(original_message: str) -> str:
-        framerecords = inspect.stack()
-        framerecord = framerecords[2]
-        file_name = framerecord[1]
-        line_number = framerecord[2]
-        func_name = framerecord[3]
-
-        return "{0}::{1}, line {2} >> {3}".format(file_name, func_name, line_number, original_message)
-
-    def debug(self, message: str, should_log_slack: bool = True):
-        message = Logger._make_message(message)
-        self.logger.debug(message)
-        if should_log_slack:
-            self._log_to_slack(logging.DEBUG, "DEBUG " + message)
-
-    def info(self, message: str, should_log_slack: bool = True):
-        message = Logger._make_message(message)
-        self.logger.info(message)
-        if should_log_slack:
-            self._log_to_slack(logging.INFO, "INFO " + message)
-
-    def warning(self, message: str, should_log_slack: bool = True):
-        message = Logger._make_message(message)
-        self.logger.warning(message)
-        if should_log_slack:
-            self._log_to_slack(logging.WARNING, "WARNING " + message)
-
-    def error(self, message: str, should_log_slack: bool = True):
-        message = Logger._make_message(message)
-        self.logger.error(message)
-        if should_log_slack:
-            self._log_to_slack(logging.ERROR, "ERROR " + message)
-
-    def critical(self, message: str, should_log_slack: bool = True):
-        message = Logger._make_message(message)
-        self.logger.critical(message)
-        if should_log_slack:
-            self._log_to_slack(logging.CRITICAL, "CRITICAL " + message)
+    return logger
